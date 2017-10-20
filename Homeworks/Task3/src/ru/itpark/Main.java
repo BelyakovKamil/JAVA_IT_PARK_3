32cd0350c413729ba2f45679137429c1c5d12040
BelyakovKamil <belyakov.kamil@mail.ru>
2017-10-20 16:47:22 +0400

    done Task3

--- Homeworks/Task3/src/ru/itpark/Main.java
+++ Homeworks/Task3/src/ru/itpark/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int sum = 0, local_max=0, max_length=1, max=0;
-        System.out.println("Введите количество элементов в ");
+        System.out.println("Введите количество элементов в массиве");
         int n = in.nextInt();
         int a[] = new int[n];
         for (int i = 0; i < n; i++) {
